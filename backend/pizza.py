8231136955a5f5b2401f7997f5e729a70afdf940
minusmochi <44338616+minusmochi@users.noreply.github.com>
2020-10-29 00:38:40 +0100

    Constructed tweet

--- backend/pizza.py
+++ backend/pizza.py
@@ -1,3 +1,6 @@
 import random
+import tweetBuilder
 toppings = ["whipped cream", "hands of lego people", "gummy bears", "toothpaste"]
-print (toppings [random.randint(0, len(toppings)-1)])
+#print (toppings [random.randint(0, len(toppings)-1)])
+topping = toppings [random.randint(0, len(toppings)-1)]
+print (tweetBuilder.constructTweet(topping))
