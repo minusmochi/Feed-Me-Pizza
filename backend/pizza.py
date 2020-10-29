4fc1f43473736c27d895aaa04cb878d3d92d8395
minusmochi <44338616+minusmochi@users.noreply.github.com>
2020-10-29 01:02:33 +0100

    Made pizza tweet callable from other script

--- backend/pizza.py
+++ backend/pizza.py
@@ -1,6 +1,9 @@
 import random
 import tweetBuilder
-toppings = ["whipped cream", "hands of lego people", "gummy bears", "toothpaste"]
-#print (toppings [random.randint(0, len(toppings)-1)])
-topping = toppings [random.randint(0, len(toppings)-1)]
-print (tweetBuilder.constructTweet(topping))
+def getPizzaTweet():
+    toppings = ["whipped cream", "hands of lego people", "gummy bears", "toothpaste"]
+    #print (toppings [random.randint(0, len(toppings)-1)])
+    topping = toppings [random.randint(0, len(toppings)-1)]
+    return tweetBuilder.constructTweet(topping)
+
+#print (getPizzaTweet())
