5fa73d19391680f08790ea473bcccea636bd1412
minusmochi <44338616+minusmochi@users.noreply.github.com>
2020-10-29 03:41:27 +0100

    Added more values to the toppings array

--- backend/pizza.py
+++ backend/pizza.py
@@ -1,7 +1,10 @@
 import random
 import tweetBuilder
 def getPizzaTweet():
-    toppings = ["whipped cream", "hands of lego people", "gummy bears", "toothpaste"]
+    toppings = ["whipped cream", "hands of lego people", "gummy bears", "toothpaste", "super glue", "fresh pubes", "motor oil", 
+    "pebbles", "wood shavings", "sticky notes", "sand", "dust mites", "oxygen", "sea shells", "daisies", "paper people",
+    "tiny Yoshis", "diamonds", "toe nails", "playdough", "bullets", "golf balls", "body lotion", "confetti", "scissors", 
+    "paper clips", "coasters", "bow ties", "flapjacks", "percy pigs", "snails", "monocles", "bibles"]
     #print (toppings [random.randint(0, len(toppings)-1)])
     topping = toppings [random.randint(0, len(toppings)-1)]
     return tweetBuilder.constructTweet(topping)
